28d9f9806c9abab49433306f84bde847750ee7b2
unknown <arkantos1569@gmail.com>
2026-02-21 14:19:34 -0600

    primer commit - proyecto Angular

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Bienvenida } from './features/bienvenida/bienvenida';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,Bienvenida],
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('tema2-app');
+}
